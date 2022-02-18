6aab6f763505099a3a22dde1e4acafcec519686d
Deepak Sharma <99655257+DeepakSharma06@users.noreply.github.com>
2022-02-18 15:09:01 +0530

    Main

---
+++ GuessTheWord.py
@@ -0,0 +1,19 @@
+# Guess the word
+
+word = 'lion'
+
+i=1
+
+while i<=3:
+
+    gu=input('Enter word: ')
+
+    i+=1
+
+    if word== gu:
+
+        print ('you win')
+
+        break
+
+    else: print('you loss')
